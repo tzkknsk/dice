5ae2486130a64df49c3ad456355bbb6eddee94e8
tzkknsk <104813025+tzkknsk@users.noreply.github.com>
2022-06-11 20:52:57 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from PIL import Image
+
+
+## dice
+# Functions
+def dice(input_face_num):
+    if input_face_num == 24:
+        return random.randint(1, 24)
+    else:
+        return random.randint(1, input_face_num)
+
+roll = 0
+
+if st.button('6面体サイコロを振る'):
+     dice6 = dice(6)
+     st.write(dice6)
+
+if st.button('24面体サイコロを振る'):
+     dice24 = dice(24)
+     st.write(dice24)
+    
